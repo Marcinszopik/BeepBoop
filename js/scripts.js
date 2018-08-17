8154b424a2fa7dafe635366fb01d26fa51b55360
andrew <amcgladd@gmail.com>
2018-08-17 09:32:04 -0700

    add spec to replace zero with beep

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 //back-end logic
+function beepBooper(userInput) {
+  if (userInput === 0) {
+    return ["Beep!"]
+  }
+}
 
 //front-end logic
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
+    var result = "";
+    result += beepBooper(userInput);
     $("#outputList").empty();
-    $("#outputList").append('<li>'+userInput+'</li>');
+    $("#outputList").append('<li>'+result+'</li>');
     $("#output").show();
   });
 });
