f6c0272452ea75afca0bf093ce0e8a523a446f63
Marcinszopik <szopiks@yahoo.com>
2020-02-24 14:53:57 -0800

    Upload index.html, css, js, and README files
    Independent Project

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ function beepBooper(number) {
   var outputArray= [];
   for (var index = 0; index < number + 1; index++) {
     var inputString = index.toString();
-    if ((index > 1) && (index % 3 === 0)){
-    outputArray.push("<li>I’m sorry, Dave. I'm afraid I can't do that</li>");
+    if ((index >= 1) && (index % 3 === 0)){
+    outputArray.push("<li>Won't you be my Neighbor?</li>");
   } else if ((index === 1) || (inputString.includes("1"))) {
     outputArray.push("<li>Boop!</li>");
   } else if ((index === 0) || (inputString.includes("0"))) {
